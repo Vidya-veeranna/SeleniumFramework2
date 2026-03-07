3d9fddc3c9b47f4132f3a50475d39008ab361337
vidya-veeranna <vidyagowda191@gmail.com>
2026-03-07 13:40:29 +0530

    added screenshot and excel utils

--- src/test/java/com/selenium/utils/ExcelUtils.java
+++ src/test/java/com/selenium/utils/ExcelUtils.java
@@ -1,5 +1,56 @@
 package com.selenium.utils;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
 public class ExcelUtils {
-    
+    private static String path = System.getProperty("user.dir")+"/resources/testdata.xlsx";
+
+    public static int getRow(String sheetname){
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            XSSFWorkbook book = new XSSFWorkbook(fis);
+            XSSFSheet sheet = book.getSheet(sheetname);
+            book.close();
+            return sheet.getLastRowNum();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static String getCellData(String sheetname, int row,int col){
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            XSSFWorkbook book = new XSSFWorkbook(fis);
+            XSSFSheet sheet = book.getSheet(sheetname);
+            book.close();
+            return sheet.getRow(row).getCell(col).toString();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void writeData(String sheetname,int row,int col,String value){
+        try{
+             FileInputStream fis = new FileInputStream(path);
+            XSSFWorkbook book = new XSSFWorkbook(fis);
+            XSSFSheet sheet = book.getSheet(sheetname);
+            sheet.getRow(row).createCell(col).setCellValue(value);
+            FileOutputStream fos = new FileOutputStream(path);
+            book.write(fos);
+            fos.close();
+            book.close();
+           
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
+
+
--- src/test/java/com/selenium/utils/ScreenshotUtils.java
+++ src/test/java/com/selenium/utils/ScreenshotUtils.java
@@ -1,5 +1,14 @@
 package com.selenium.utils;
 
 public class ScreenshotUtils {
-    
+    public static String getScreenShot(WebDriver driver, String name){
+        String path = System.getProperty("user.dir")+"/screenshots/"+name+".png";
+
+        File src = ((TakeScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try{
+            FileUtils.copyFile(src,new File(path));
+        }catch(Exception e){
+            e.printStackTrace();
+    }
+    return path;
 }
